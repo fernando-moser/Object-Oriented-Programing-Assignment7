8ecbe5c75b586253a39c8426d9b093f6f0ad4106
fernando-moser <fernando.moser.pereira@hotmail.com>
2020-03-18 09:39:06 -0300

    initial commit, classes Drink and Coffee done

---
+++ Coffee.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab7_v2
+{
+    public class Coffee : Drink
+    {
+        public bool Sugar;
+        public bool Cream;
+
+        public Coffee(string name, bool hot, double size)
+            : base(name, hot, size) { }
+
+        public Coffee(string name, bool hot, double size, bool sugar, bool cream)
+            : this(name, hot, size)
+        {
+            Sugar = sugar;
+            Cream = cream;
+        }
+
+        public override string Prepare() => $" {base.Prepare()} \n grounding {Name} beans \n {Heat()} \n adding hotwater";
+
+        public override string Heat() => $" {base.Heat()} water";
+
+        public override string Serve() => $" pouring {Name} in a mug \n putting lid \n putting holder \n {base.Serve()}";
+
+    }
+}
---
+++ Drink.cs
@@ -0,0 +1,26 @@
+﻿namespace Lab7_v2
+{
+    public class Drink
+    {
+        public string Name;
+        public bool Hot;
+        public double Size;
+
+        public Drink() => Size = 300.0;
+
+        public Drink(string name, bool hot, double size)
+        {
+            Name = name;
+            Hot = hot;
+            Size = Size > 150 ? Size = size : 300.0;
+        }
+
+        public virtual string Prepare() => $"Preparing {Name}";
+
+        public virtual string Heat() => $"Heating {Name}";
+
+        public virtual string Serve() => $"Serving {Name}";
+
+
+    }
+}
